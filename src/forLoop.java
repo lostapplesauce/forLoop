29515177a9a046582b80798c3c79ea9106dc12f6
troyturner <troyturner444@gmail.com>
2020-03-31 16:31:17 -0700

    working

--- src/forLoop.java
+++ src/forLoop.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
+
 public class forLoop {
+
+    private static String[] practice(){
+        String apple[] = {"rose","fiji","red","poison",
+        "special","giant"};
+        System.out.print(Arrays.toString(apple));
+      return apple;
+    }
     public static void main(String[] args){
+
         for(int i = 1; i<= 25; i++) {
             for (int j = 0; j <= 25; j += 2) {
                 System.out.println(i);
                 System.out.println(j);
             }
+        }
+        for(int k=0; k<practice().length; k++){
+            System.out.println(Arrays.toString(practice()));
         }
     }
 }
